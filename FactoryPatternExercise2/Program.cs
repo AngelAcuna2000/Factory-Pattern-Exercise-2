d34f5884ab3c8dc6dd6411eefe08f0fc4ed39b83
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-31 23:37:56 -0400

    Exercise Complete

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What type of database would you like to use: List, SQL, or Mongo?");
+            var databaseType = Console.ReadLine();
+
+            IDataAccess database = DataAccessFactory.GetDataAccessType(databaseType);
+
+            List<Product> products = database.LoadData();
+            database.SaveData();
         }
     }
 }
